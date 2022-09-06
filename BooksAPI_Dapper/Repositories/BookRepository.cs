7c611e551fbe6db036caf6a788761ad88e4d2c35
Deepanjan Nag <subscriptions@deepanjannag.com>
2022-09-05 21:13:07 -0400

    Initial Commit

---
+++ BooksAPI_Dapper/Repositories/BookRepository.cs
@@ -0,0 +1,89 @@
+﻿using BooksAPI_Dapper.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace BooksAPI_Dapper.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+
+        private readonly string _connectionString;
+
+        public BookRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("BooksConnectionString");
+        }
+
+        public async Task<Book> Create(Book book)
+        {
+            var sqlQuery = "INSERT INTO Books(Title, Author, Description) VALUES (@Title, @Author, @Description)";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new { book.Title, book.Author, book.Description});
+            }
+            return book;
+        }
+
+        public async Task Delete(int id)
+        {
+            var sqlQuery = "DELETE FROM Books WHERE Id=@Id";
+
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new { Id=id });
+            }
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            var sqlQuery = "SELECT * FROM Books";
+            var books = new List<Book>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //using (var sqlCommand  = new SqlCommand(sqlQuery, connection))
+                //{
+                //    connection.Open();
+
+                //    using (var reader = await sqlCommand.ExecuteReaderAsync())
+                //    {
+                //        while (reader.Read())
+                //        {
+                //            books.Add(new Book { Id = reader.GetInt32(0), Author= reader.GetString(1), Title=reader.GetString(2), Description=reader.GetString(3)});
+                //        }
+                //    }
+                //}
+                //return books;
+
+                //Above ADO.NET is being replaced by DAPPER
+                return await connection.QueryAsync<Book>(sqlQuery);
+
+            }
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            var sqlQuery = "SELECT * FROM Books WHERE Id=@BookId";
+            var book = new Book();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return await connection.QueryFirstOrDefaultAsync<Book>(sqlQuery, new {BookId = id});
+            }
+        }
+
+        public async Task Update(Book book)
+        {
+            var sqlQuery = "UPDATE Books SET Title=@Title, Author=@Author, Description=@Description WHERE Id=@Id";
+            
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new {book.Id, book.Title, book.Author, book.Description});
+            }
+        }
+    
+    }
+}
---
+++ BooksAPI_Dapper/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BooksAPI_Dapper.Models;
+
+namespace BooksAPI_Dapper.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
